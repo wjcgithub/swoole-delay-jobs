c155c9f56d141156a7fa7cb877551be771cc3b8e
wjcgithub <wjc163@sina.cn>
2017-04-07 17:49:12 +0800

    添加启动文件

--- start.php
+++ start.php
@@ -5,3 +5,7 @@
  * Date: 17-4-7
  * Time: 上午11:04
  */
+date_default_timezone_set('Asia/Shanghai');
+require __DIR__ . '/vendor/autoload.php';
+$config = parse_ini_file('./config.ini', true);
+$server = new \Evolution\DJob\Process($config);
