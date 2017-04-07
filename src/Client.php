162b7721e2fb459a22e34226244b0c01f8c3b522
wjcgithub <wjc163@sina.cn>
2017-04-07 17:48:50 +0800

    add client

--- src/Client.php
+++ src/Client.php
@@ -11,5 +11,18 @@ namespace Evolution\DJob;
 
 class Client
 {
+    private $queue=null;
+    private $config=[];
 
+    public function __construct($config)
+    {
+        $this->config = $config;
+        $this->queue = new Redis($this->config['queue'][$this->config['queue']['default']]);
+    }
+
+    public function pushToSolt(array $info)
+    {
+        $delayTime = $info['delayTime'];
+        $json = $info['json'];
+    }
 }
