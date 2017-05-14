484a501d526e2e3d3fa572c94627f309d5599c55
wjcgithub <wjc163@sina.cn>
2017-05-14 19:49:42 +0800

    修改新的测试数据

--- test.php
+++ test.php
@@ -7,5 +7,6 @@
  */
 date_default_timezone_set('Asia/Shanghai');
 require __DIR__ . '/vendor/autoload.php';
-$obj = new \Evolution\TimerWheel\Timer\TimingWheel(1000,3600);
+$config = parse_ini_file('./config.ini', true);
+$obj = new \Evolution\TimerWheel\Timer\TimingWheel($config);
 $obj->start();
--- test/TestPush.php
+++ test/TestPush.php
@@ -10,13 +10,17 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 $config = parse_ini_file('./config.ini', true);
 $client = new \Evolution\DJob\Client($config);
 
+$redis = new \Evolution\DJob\Storage\Queue\Redis(['host'=>'127.0.0.1','port'=>6379]);
 for ($i=0; $i<20000; $i++) {
-    $dtime = rand(30,60);
+    $dtime = rand(10,6000);
     $info = [
-        'delayTime' => $dtime,
+        'type' => 'delay',
+        'isrepeat' => 0,
+        'exectime' => $dtime,
         'json' => ['name'=>'lisi', 'sex'=>'zhangsan']
     ];
-    $client->pushToSolt($info);
-    echo $dtime."\n";
+
+    $redis->lpush('delayqueue', json_encode($info));
+    echo json_encode($info)."\r\n";
     sleep(1);
 }
